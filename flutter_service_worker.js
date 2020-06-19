326e4a6d1d1ccf5c519038cb798203268bbb8d03
William Terrill <william.terrill@gmail.com>
2020-06-19 14:06:09 -0500

    got rid of auditors due to minified issue with Hive

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "63aaaa5fc1bfefb67446070a68e81d2b",
 "/": "63aaaa5fc1bfefb67446070a68e81d2b",
-"main.dart.js": "bfcdcd8369f402865724e07b9f087986",
+"main.dart.js": "c02683380d23a09541e0177a17e8e2cf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
